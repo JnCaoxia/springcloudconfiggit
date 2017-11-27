11962255b3e01ef17830aea494f1fde4447e77fc
caoxia <nazi@baofu.com>
2017-11-27 22:22:08 +0800

    commit

---
+++ springcloudconfiggit/src/main/java/com/caox/SpringCloudConfigGitApplication.java
@@ -0,0 +1,12 @@
+package com.caox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigGitApplication.class, args);
+	}
+}
