809f244f658f82effb66065bcb21a1551c641dbf
caoxia <nazi@baofu.com>
2017-11-28 17:08:05 +0800

    client 加入refresh

--- springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
+++ springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
@@ -1,14 +1,16 @@
 package com.caox.web;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope // 使用该注解的类，会在接到SpringCloud配置中心配置刷新的时候，自动将新的配置更新到该类对应的字段中
 public class HelloController {
 
     /**
-     *
+     *  加入配置刷新
      */
     @Value("${neo.hello}")
     private String hello;
