fbab918655c78da1efab001747efb42da56ce566
caoxia <nazi@baofu.com>
2017-11-28 15:54:12 +0800

    update

--- springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
+++ springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class HelloController {
+public class HelloController {
 
     @Value("${neo.hello}")
     private String hello;
--- springcloudconfigclient/src/test/java/com/caox/SpringCloudConfigClientApplicationTests.java
+++ springcloudconfigclient/src/test/java/com/caox/SpringCloudConfigClientApplicationTests.java
@@ -2,6 +2,7 @@ package com.caox;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringCloudConfigClientApplicationTests {
 
+
+	@Value("${neo.hello}")
+	private  String  hello;
+
 	@Test
 	public void contextLoads() {
+		System.out.println("************************************************************");
+		System.out.println("hello value : "+hello);
+		System.out.println("************************************************************");
 	}
-
 }
