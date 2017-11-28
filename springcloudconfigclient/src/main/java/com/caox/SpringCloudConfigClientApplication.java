41d7a031fbcd0d31b026f2e0a32e6ee4656b2879
caoxia <nazi@baofu.com>
2017-11-28 15:07:50 +0800

    配置中心git示例 spring-cloud-config-client

--- springcloudconfigclient/src/main/java/com/caox/SpringCloudConfigClientApplication.java
+++ springcloudconfigclient/src/main/java/com/caox/SpringCloudConfigClientApplication.java
@@ -2,13 +2,11 @@ package com.caox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class SpringCloudConfigServerApplication {
+public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
 }
