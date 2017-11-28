fce03e2c10506958a9064e58010996a879642842
caoxia <nazi@baofu.com>
2017-11-28 15:56:22 +0800

    update

--- springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
+++ springcloudconfigclient/src/main/java/com/caox/web/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    /**
+     *
+     */
     @Value("${neo.hello}")
     private String hello;
 
