ff01c4dfebba79125dbe198e776312878aaef3ed
caoxia <nazi@baofu.com>
2017-12-12 14:52:14 +0800

    服务网关zuul  通过url映射的方式来实现zull的转发有局限性，比如每增加一个服务就需要配置一条内容，另外后端的服务如果是动态来提供，就不能采用这种方案来配置了。实际上在实现微服务架构时，服务名与服务实例地址的关系在eureka server中已经存在了，所以只需要将Zuul注册到eureka server上去发现其他服务，就可以实现对serviceId的映射

---
+++ springcloudproducer2/src/main/java/com/caox/SpringCloudProducer2Application.java
@@ -0,0 +1,14 @@
+package com.caox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudProducer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudProducer2Application.class, args);
+	}
+}
