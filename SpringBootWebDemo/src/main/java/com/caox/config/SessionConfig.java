1e2efc09832755ea2aae0ebaf38a86b059e37555
caoxia <nazi@baofu.com>
2017-12-19 16:38:25 +0800

    (三)Spring boot中Redis的使用

--- SpringBootWebDemo/src/main/java/com/caox/config/SessionConfig.java
+++ SpringBootWebDemo/src/main/java/com/caox/config/SessionConfig.java
@@ -5,6 +5,7 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 86400*30)
+// maxInactiveIntervalInSeconds: 设置Session失效时间，使用Redis Session之后，原Boot的server.session.timeout属性不再生效
 public class SessionConfig {
 	
 }
