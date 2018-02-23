41410de42076eb2973904e2ede69c33031cedf56
caoxia <nazi@baofu.com>
2018-02-23 16:38:17 +0800

    springboot(十七)：使用Spring Boot上传文件

---
+++ SpringBootMongoDB/src/main/java/com/caox/SpringBootPackageWarApplication.java
@@ -0,0 +1,12 @@
+package com.caox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPackageWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPackageWarApplication.class, args);
+	}
+}
