10ca57d70c15dcd5dc3156f202aad76b890bc41b
caoxia <nazi@baofu.com>
2018-03-13 13:58:06 +0800

    springboot(二十)：使用spring-boot-admin对spring-boot服务进行监控

---
+++ SpringBootAdminProducer/src/main/java/com/caox/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.caox.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello one producer"+name+"，this is first message!";
+    }
+}
