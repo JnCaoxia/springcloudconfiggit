a43f35a627e88de42e02b0d3ba9f33d10008f792
caoxia <nazi@baofu.com>
2018-03-13 15:31:43 +0800

    Spring Boot 2.0 新特性（二）：新增事件ApplicationStartedEvent

---
+++ SpringBootApplicationEventsAndListeners/src/main/java/com/example/demo/SpringBootApplicationEventsAndListenersApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationEventsAndListenersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationEventsAndListenersApplication.class, args);
+	}
+}
